63952e531d02619a84a0c17f22af394149879250
phuocvinhthuynguyen <147361586+phuocvinhthuynguyen@users.noreply.github.com>
2023-12-14 23:02:42 +0700

    Add files via upload

---
+++ PlayFair/PlayFair/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PlayFair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(478, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PLAYFAIR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plaintext";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Key";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 588);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ciphertext";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 83);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(520, 92);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 197);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(520, 29);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(159, 585);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(522, 92);
+            this.textBox3.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(72, 314);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(609, 248);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Key matrix";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(748, 83);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(261, 120);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Type";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 57);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Decrypt";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Encrypt";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(772, 237);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(105, 24);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "5x5 matrix";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(772, 267);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(105, 24);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "6x6 matrix";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 54);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Create key matrix";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(748, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 54);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Execute";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 726);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PlayFair/PlayFair/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayFair
+{
+    public partial class Form1 : Form
+    {
+        private char[,] keyMatrix;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = textBox2.Text.Trim().ToUpper();
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; // Loại bỏ chữ 'J'
+
+            if (string.IsNullOrEmpty(key))
+            {
+                keyMatrix = GenerateMatrix(alphabet);
+            }
+            else
+            {
+                key = key.Replace("J", "I");
+                string keyWithoutDuplicates = new string(key.Distinct().ToArray());
+                string keyWithRemainingAlphabet = keyWithoutDuplicates + alphabet;
+
+                keyMatrix = GenerateMatrix(keyWithRemainingAlphabet);
+            }
+
+            DisplayKeyMatrix();
+        }
+        private char[,] GenerateMatrix(string key)
+        {
+            char[,] matrix = new char[5, 5];
+            int keyIndex = 0;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    matrix[row, col] = key[keyIndex];
+                    keyIndex++;
+                }
+            }
+
+            return matrix;
+        }
+
+        private void DisplayKeyMatrix()
+        {
+            groupBox1.Controls.Clear();
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Text = keyMatrix[row, col].ToString();
+                    textBox.ReadOnly = true;
+                    textBox.Width = 30;
+                    textBox.TextAlign = HorizontalAlignment.Center;
+
+                    groupBox1.Controls.Add(textBox);
+                    groupBox1.SetCellPosition(textBox, new TableLayoutPanelCellPosition(col, row));
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                string plaintext = textBox1.Text.Trim().ToUpper();
+                string ciphertext = Encrypt(plaintext);
+                textBox3.Text = ciphertext;
+            }
+            else if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+                string ciphertext = textBox3.Text.Trim().ToUpper();
+                string plaintext = Decrypt(ciphertext);
+                textBox1.Text = plaintext;
+            }
+        }
+        private string Encrypt(string plaintext)
+        {
+            StringBuilder ciphertext = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length; i += 2)
+            {
+                char char1 = plaintext[i];
+                char char2 = (i + 1 < plaintext.Length) ? plaintext[i + 1] : 'X';
+
+                int row1 = -1, col1 = -1, row2 = -1, col2 = -1;
+                FindCharacter(char1, out row1, out col1);
+                FindCharacter(char2, out row2, out col2);
+
+                if (row1 == row2)
+                {
+                    ciphertext.Append(keyMatrix[row1, (col1 + 1) % 5]);
+                    ciphertext.Append(keyMatrix[row2, (col2 + 1) % 5]);
+                }
+                else if (col1 == col2)
+                {
+                    ciphertext.Append(keyMatrix[(row1 + 1) % 5, col1]);
+                    ciphertext.Append(keyMatrix[(row2 + 1) % 5, col2]);
+                }
+                else
+                {
+                    ciphertext.Append(keyMatrix[row1, col2]);
+                    ciphertext.Append(keyMatrix[row2, col1]);
+                }
+            }
+
+            return ciphertext.ToString();
+        }
+
+        private string Decrypt(string ciphertext)
+        {
+            StringBuilder plaintext = new StringBuilder();
+
+            for (int i = 0; i < ciphertext.Length; i += 2)
+            {
+                char char1 = ciphertext[i];
+                char char2 = (i + 1 < ciphertext.Length) ? ciphertext[i + 1] : 'X';
+
+                int row1 = -1, col1 = -1, row2 = -1, col2 = -1;
+                FindCharacter(char1, out row1, out col1);
+                FindCharacter(char2, out row2, out col2);
+
+                if (row1 == row2)
+                {
+                    plaintext.Append(keyMatrix[row1, (col1 + 4) % 5]);
+                    plaintext.Append(keyMatrix[row2, (col2 + 4) % 5]);
+                }
+                else if (col1 == col2)
+                {
+                    plaintext.Append(keyMatrix[(row1 + 4) % 5, col1]);
+                    plaintext.Append(keyMatrix[(row2 + 4) % 5, col2]);
+                }
+                else
+                {
+                    plaintext.Append(keyMatrix[row1, col2]);
+                    plaintext.Append(keyMatrix[row2, col1]);
+                }
+            }
+
+            return plaintext.ToString();
+        }
+
+        private void FindCharacter(char c, out int row, out int col)
+        {
+            row = -1;
+            col = -1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (keyMatrix[i, j] == c)
+                    {
+                        row = i;
+                        col = j;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
